7b1d05008a4a2b92845b2de405d425ef4ab79be0
Richard Wang <rwang83@gmail.com>
2023-06-08 01:44:10 -0400

    update

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,13 +1,13 @@
 import { endPoints } from '@/common/config'
-import http, { IncomingMessage, ServerResponse } from 'http'
 import path from 'path'
 import { serveFolder } from './fileserver'
-import httpProxy from 'http-proxy';
+import httpProxy from 'http-proxy'
+import express from 'express'
 
 const args = process.argv.slice(2)
 const port = args[0]
 
-const proxy = httpProxy.createProxyServer();
+const proxy = httpProxy.createProxyServer()
 const clientJS = path.join(process.cwd(), 'build', 'client')
 const captures = path.join(process.cwd(), endPoints.captures)
 
@@ -18,52 +18,34 @@ const serveCaptures = serveFolder({
   useCache: false
 })
 
-const handlers: {
-  [s: string]: (req: IncomingMessage, res: ServerResponse, matched: string) => boolean
-} = {}
+const app = express()
 
-handlers[endPoints.hello] = (req, res, m) => {
+app.get(endPoints.hello, (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
   res.end('Hello from server!!')
-  return true
-}
-
-handlers[endPoints.stream] = (req, res, m) => {
-  const target = `http://localhost:8083/${req.url?.substring(m.length + 1)}`
-  console.log('target', target)
-  proxy.web(req, res, { target });
-  return true
-}
-
-handlers[''] = (req, res) => serveCaptures(req, res) || serveClientJS(req, res)
-
-const handlerKeys = Object.keys(handlers)
-const server = http.createServer((req, res) => {
-  try {
-    if (req.url === '' || req.url === '/') {
-      res.writeHead(302, {
-        Location: '/index.html'
-      })
-      res.end()
-      return
-    }
-
-    for (const key of handlerKeys) {
-      if (req.url?.startsWith(key)) {
-        const handled = handlers[key](req, res, key)
-        if (handled) return
-      }
-    }
-
-    // default handler
-    res.writeHead(404, { 'Content-Type': 'text/plain' })
-    res.end('File not found!')
-    return
-  } catch (e) {
-    console.log(e)
-  }
 })
 
-server.listen(port, () => {
+app.get(endPoints.stream, (req, res) => {
+  const url = req.url?.substring(endPoints.stream.length + 1)
+  const target = `http://localhost:8083/${url}`
+  proxy.web(req, res, { target })
+})
+
+app.get('/', (req, res) => {
+  res.redirect('/index.html')
+})
+
+app.use((req, res) => {
+  const handled =
+    serveCaptures(req, res) ||
+    serveClientJS(req, res) ||
+    (() => {
+      res.writeHead(404, { 'Content-Type': 'text/plain' })
+      res.end('File not found!')
+      return true
+    })()
+})
+
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
